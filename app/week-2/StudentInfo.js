991f8d4fd2ee149c1926645abbd31cde26305e31
RuriSora <hoatran9592@hotmail.com>
2026-01-22 22:15:57 -0700

    Update Page(Home with Link to Week 2 to 8 and Week 2 link lead to Page Week 2) + Page(Week-2 Import Link and adding the correct StudentInfor Component) and StudentInfor (Link to show as example)

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <h1>Hoa Tran</h1>
+            <Link href="https://github.com/Rurisora/cprg306-assignment2">GitHub: https://github.com/Rurisora/cprg306-assignment2</Link>
+        </div>
+    )
+}
