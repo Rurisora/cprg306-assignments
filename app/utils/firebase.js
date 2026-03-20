58053c621439c003088e2ba45e2b7032d6e316b1
RuriSora <hoatran9592@hotmail.com>
2026-03-20 09:33:11 -0600

    rework on firebase.

--- app/utils/firebase.js
+++ app/utils/firebase.js
@@ -1,22 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+import { getAuth } from "firebase/auth";
+ 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCKLMbl7NJUNis9dHELyYugdxrpuA5srtY",
-  authDomain: "cprg306-assignments-63f88.firebaseapp.com",
-  projectId: "cprg306-assignments-63f88",
-  storageBucket: "cprg306-assignments-63f88.firebasestorage.app",
-  messagingSenderId: "809761261980",
-  appId: "1:809761261980:web:02a7a83a563449ff97e059"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
-
+ 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-//export auth to use in AuthContext
 export const auth = getAuth(app);
-
